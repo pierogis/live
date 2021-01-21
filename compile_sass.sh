a4921fd84f4d3562841226035ff943b9d70bdcfd
Kyle Moore <moore.kyle39@gmail.com>
2021-01-20 22:36:54 -0500

    use sass directly

--- compile_sass.sh
+++ compile_sass.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd src/pierogis_live
-sass --load-path=static/sass \
+dart-sass/sass --load-path=static/sass \
     static/sass:static/css \
     blueprints/content/static/sass:blueprints/content/static/css \
     blueprints/trial/static/sass:blueprints/trial/static/css
