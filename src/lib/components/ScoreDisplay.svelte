a0c6d59fdac4141fddca37d3e7ee19b763201828
Kyle Moore <moore.kyle39@gmail.com>
2022-02-24 03:39:01 -0500

    add working scores toggle function

--- src/lib/components/ScoreDisplay.svelte
+++ src/lib/components/ScoreDisplay.svelte
@@ -8,16 +8,17 @@
 	export let score: Score;
 
 	enum ScoreStatus {
-		full = '★',
-		half = '½',
-		empty = '☆'
+		full = '●',
+		half = '◐',
+		empty = '○'
 	}
 
-	let fullScores = Math.floor(score.value);
-	let halfScores = Math.round((score.value - fullScores) / 0.5) % 2;
-	let emptyScores = 5 - (fullScores + halfScores);
+	$: fullScores = Math.floor(score.value);
+	$: halfScores = Math.round((score.value - fullScores) / 0.5) % 2;
+	$: emptyScores = 5 - (fullScores + halfScores);
 
-	let scoreStatuses: ScoreStatus[] = Array(fullScores)
+	let scoreStatuses: ScoreStatus[];
+	$: scoreStatuses = Array(fullScores)
 		.fill(ScoreStatus.full)
 		.concat(Array(halfScores).fill(ScoreStatus.half))
 		.concat(Array(emptyScores).fill(ScoreStatus.empty));
@@ -26,12 +27,10 @@
 		element: HTMLElement,
 		params: { scoreStatus: ScoreStatus; i: number }
 	) {
-		function handleClick(event) {
-			event.stopPropagation();
-			console.log(event);
+		function handlePointerDown(event) {
 			if (Math.ceil(score.value) == params.i + 1) {
-				if (score.value == params.i) {
-					score.value = params.i + 1.5;
+				if (score.value == params.i + 1) {
+					score.value = params.i + 0.5;
 				} else if (score.value == params.i + 0.5) {
 					score.value = params.i;
 				} else {
@@ -42,25 +41,39 @@
 			}
 		}
 
-		element.addEventListener('pointerdown', handleClick);
+		element.addEventListener('pointerdown', handlePointerDown);
 
 		return {
 			update(newParams: { scoreStatus: ScoreStatus; i: number }) {
 				params = newParams;
 			},
 			destroy() {
-				element.removeEventListener('click', handleClick);
+				element.removeEventListener('click', handlePointerDown);
 			}
 		};
 	}
 </script>
 
-{#each scoreStatuses as scoreStatus, i (i)}
-	<span class="score" use:changeScoreAction={{ scoreStatus, i }}>{scoreStatus}</span>
-{/each}
+<div class="scores-container">
+	{#each scoreStatuses as scoreStatus, i (i)}
+		<span class="score" use:changeScoreAction={{ scoreStatus, i }}>{scoreStatus} </span>
+	{/each}
+</div>
 
 <style>
 	.score {
 		cursor: pointer;
+		-webkit-user-select: none;
+		user-select: none;
+
+		font-size: 1.1em;
+		padding: 4px;
+	}
+
+	.scores-container {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		flex: 2;
 	}
 </style>
--- src/lib/components/Scores.svelte
+++ src/lib/components/Scores.svelte
@@ -5,25 +5,31 @@
 
 	export let scores: Scores;
 
-	const emojis = {
-		identifiability: '👁️',
-		colors: '🎨',
-		symbols: '💫',
-		typeface: '🔤',
-		clarity: '👓'
+	const categories = {
+		identifiability: { emoji: '👁️' },
+		colors: { emoji: '🎨' },
+		symbols: { emoji: '💫' },
+		typeface: { emoji: '🔤' },
+		clarity: { emoji: '👓' }
 	};
 </script>
 
 <div style="height: 4px;" />
 <div class="scores">
-	<span aria-describedby="starsSummary"><ScoreDisplay score={scores.overall} /></span>
-	<div role="tooltip" id="starsSummary">
+	<span aria-describedby="starsSummary">
 		<ScoreDisplay score={scores.overall} />
+	</span>
+	<div role="tooltip" class="scoresheet" id="starsSummary">
+		<ScoreDisplay bind:score={scores.overall} />
 		<div class="overall-seperator" />
 		{#each Object.entries(scores) as [name, category]}
 			{#if name != 'overall'}
-				<span>{emojis[name]}: <ScoreDisplay score={category} /></span>
-				<br />
+				<div class="category">
+					<span class="emoji" title={name}>{categories[name].emoji} </span>
+					<ScoreDisplay score={category} />
+					<div style="flex: 1;" />
+					<br />
+				</div>
 			{/if}
 		{/each}
 	</div>
@@ -35,24 +41,36 @@
 		display: inline-block;
 		font-size: 1em;
 		font-family: monospace;
+
+		user-select: none;
+	}
+
+	.category {
+		display: flex;
 	}
 
 	[role='tooltip'] {
 		visibility: hidden;
 		position: absolute;
 		z-index: 1;
-		background-color: var(--primary-color);
 
-		box-shadow: 4px 4px 0 var(--accent-color), -4px -4px 0 var(--secondary-color),
-			0px 0px 10px 2px rgba(0, 0, 0, 0.4);
+		text-align: center;
 
-		width: 112px;
+		-webkit-user-select: none;
+		user-select: none;
+	}
+
+	.scoresheet {
+		width: 80%;
 		top: 0%;
 		left: 50%;
-		margin-left: -56px;
-		text-align: center;
-		font-family: monospace;
+		margin-left: -40%;
+
+		background-color: var(--primary-color);
+		box-shadow: 4px 4px 0 var(--accent-color), -4px -4px 0 var(--secondary-color),
+			0px 0px 10px 2px rgba(0, 0, 0, 0.4);
 	}
+
 	@media (hover: hover) and (pointer: fine) {
 		[aria-describedby]:hover,
 		[aria-describedby]:focus {
@@ -67,5 +85,9 @@
 	}
 	.overall-seperator {
 		border-bottom: 2px solid var(--text-color);
+	}
+	.emoji {
+		cursor: help;
+		flex: 1;
 	}
 </style>
