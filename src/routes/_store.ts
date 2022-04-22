3613e6db1793f88bbacc25dce54a2324f949e7b8
Kyle Moore <moore.kyle39@gmail.com>
2022-04-22 03:51:32 -0400

    add localStorage utility, add, remove and export

---
+++ src/routes/_store.ts
@@ -0,0 +1,20 @@
+import { writable, type Writable } from 'svelte/store';
+import { browser } from '$app/env';
+
+export const paletteStore: Writable<string> = writable(
+	browser ? localStorage.getItem('palette') || '' : ''
+);
+export const colorsStore: Writable<string[]> = writable(
+	browser ? (localStorage.getItem('colors') ? JSON.parse(localStorage.getItem('colors')) : []) : []
+);
+
+paletteStore.subscribe((palette) => {
+	if (browser) {
+		localStorage.setItem('palette', palette);
+	}
+});
+colorsStore.subscribe((colors) => {
+	if (browser) {
+		localStorage.setItem('colors', JSON.stringify(colors));
+	}
+});
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,28 +1,61 @@
 <script lang="ts">
-	let colorHexes: string;
-	let colors: string[] = [];
+	import { colorsStore, paletteStore } from './_store';
 
-	function handleClick() {
-		console.log(colorHexes);
-		colors = colorHexes.split(' ');
+	function handleImport() {
+		$colorsStore = $paletteStore.split(' ');
+	}
+
+	function handleAddColor() {
+		$colorsStore = [...$colorsStore, '000000'];
+	}
+
+	function handleRemoveColor(i: number) {
+		$colorsStore.splice(i, 1);
+		$colorsStore = $colorsStore;
+	}
+
+	function handleExport() {
+		$paletteStore = $colorsStore.join(' ');
 	}
 </script>
 
-<form>
-	<input type="text" bind:value={colorHexes} />
-	<button type="submit" on:click|preventDefault={handleClick}>submit</button>
-</form>
-<br />
-<div class="colors">
-	{#each colors as color}
-		<div class="color-container">
-			<div class="color" style:background-color="#{color}" />
-			{color}
-		</div>
-	{/each}
+<div class="content">
+	<form>
+		<textarea type="text" bind:value={$paletteStore} />
+		<button type="submit" on:click|preventDefault={handleImport}>import</button>
+	</form>
+	<br />
+	<div class="colors">
+		{#each $colorsStore as color, i}
+			<div class="color-container">
+				<div class="color" style:background-color="#{color}">
+					<span class="remove" on:click|preventDefault={() => handleRemoveColor(i)}>x</span>
+				</div>
+				<input class="color-input" bind:value={color} />
+			</div>
+		{/each}
+	</div>
+
+	<br />
+	<div class="color-container">
+		<button type="submit" on:click|preventDefault={handleAddColor}>+</button>
+	</div>
+
+	<br />
+	{#if $colorsStore.length > 0}
+		<button type="submit" on:click|preventDefault={handleExport}>export</button>
+	{/if}
 </div>
 
 <style>
+	.content {
+		padding: 4rem;
+
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+	}
 	.colors {
 		display: flex;
 		flex-direction: row;
@@ -38,7 +71,19 @@
 		align-items: center;
 	}
 	.color {
+		position: relative;
 		height: 4rem;
 		width: 4rem;
+	}
+	.color-input {
+		width: 3rem;
+	}
+	.remove {
+		position: absolute;
+		top: 0.4rem;
+		right: 0.4rem;
+
+		color: white;
+		cursor: pointer;
 	}
 </style>
