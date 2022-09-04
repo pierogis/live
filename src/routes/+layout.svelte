950a49ddbf6d1bdd45ddc6d79a2c7e3ee05f9c80
Kyle Moore <moore.kyle39@gmail.com>
2022-09-04 12:38:03 -0400

    change first link in footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,7 +6,7 @@
 	<slot />
 
 	<footer slot="footer">
-		<a class="border shadow inset link-box" href="https://emporium.pierogis.live">emporium</a>
+		<a class="border shadow inset link-box" href="https://pierogis.live">live</a>
 		<a class="border shadow inset link-box" href="https://github.com/pierogis/careers">github</a>
 		<a class="border shadow inset link-box" href="https://twitter.com/pierogis_live">
 			@pierogis_live
