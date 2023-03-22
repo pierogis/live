e4822771f4982633cc7319a2e9aea0557de2e12b
Kyle Moore <moore.kyle39@gmail.com>
2023-03-22 12:43:52 -0400

    fix all packaging stuff

--- svelte.config.js
+++ svelte.config.js
@@ -1,19 +1,14 @@
 import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
 
-import mm from 'micromatch';
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: preprocess(),
+    preprocess: preprocess(),
 
-	kit: {
-		adapter: adapter()
-	},
+    kit: {
+        adapter: adapter()
+    },
 
-	package: {
-		files: mm.matcher('!(demo/**|.DS_Store)')
-	}
 };
 
 export default config;
