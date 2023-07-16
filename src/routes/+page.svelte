4bae06d832c6d7879d83c9e4d33c2cdfe3845f5a
Kyle Moore <moore.kyle39@gmail.com>
2023-07-15 20:23:45 -0400

    update utensils

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,52 +1,85 @@
 <script lang="ts">
-	import { Card, CardsGrid, Layout, ThemeToggle } from '@pierogis/utensils';
+	import { Card, CardsGrid, Interactable, Layout, ThemeToggle } from '@pierogis/utensils';
 
 	export let data;
 </script>
 
-<Layout title="pierogis live">
+<svelte:head>
+	<title>pierogis live</title>
+</svelte:head>
+
+<Layout>
+	<Interactable slot="title">
+		<div class="link-box border inset">pierogis live</div>
+	</Interactable>
+
 	<h3 class="tagline">{data.tagline}</h3>
 
 	<CardsGrid>
-		<Card>
-			<a class="border shadow inset link-box" href="https://kitchen.pierogis.live">kitchen</a>
-			<span>work on a recipe</span>
-			<iframe title="kitchen" src="https://kitchen.pierogis.live" />
-			<br />
-		</Card>
-		<Card>
-			<a class="border shadow inset link-box" href="https://emporium.pierogis.live">emporium</a>
-			<span>a place for plates</span>
-			<iframe title="emporium" src="https://emporium.pierogis.live" />
-			<br />
-		</Card>
-		<Card>
-			<a class="border shadow inset link-box" href="https://colors.pierogis.live">colors</a>
-			<span>refine your palette</span>
-			<iframe title="colors" src="https://colors.pierogis.live" />
-			<br />
-		</Card>
-		<Card>
-			<a class="border shadow inset link-box" href="https://utensils.pierogis.live">utensils</a>
-			<span>some implements to aid consumption</span>
-			<iframe title="utensils" src="https://utensils.pierogis.live" />
-			<br />
-		</Card>
+		<Interactable clickable={false}>
+			<Card>
+				<Interactable>
+					<a class="border inset link-box" href="https://kitchen.pierogis.live">kitchen</a>
+				</Interactable>
+				<span>work on a recipe</span>
+				<iframe title="kitchen" src="https://kitchen.pierogis.live" />
+				<br />
+			</Card>
+		</Interactable>
+
+		<Interactable clickable={false}>
+			<Card>
+				<Interactable>
+					<a class="border inset link-box" href="https://emporium.pierogis.live">emporium</a>
+				</Interactable>
+				<span>a place for plates</span>
+				<iframe title="emporium" src="https://emporium.pierogis.live" />
+				<br />
+			</Card>
+		</Interactable>
+
+		<Interactable clickable={false}>
+			<Card>
+				<Interactable>
+					<a class="border inset link-box" href="https://colors.pierogis.live">colors</a>
+				</Interactable>
+				<span>refine your palette</span>
+				<iframe title="colors" src="https://colors.pierogis.live" />
+				<br />
+			</Card>
+		</Interactable>
+
+		<Interactable clickable={false}>
+			<Card>
+				<Interactable>
+					<a class="border inset link-box" href="https://utensils.pierogis.live">utensils</a>
+				</Interactable>
+				<span>some implements to aid consumption</span>
+				<iframe title="utensils" src="https://utensils.pierogis.live" />
+				<br />
+			</Card>
+		</Interactable>
 	</CardsGrid>
 
 	<br />
 
 	<footer slot="footer">
-		<a class="border shadow inset link-box" href="https://careers.pierogis.live">careers</a>
-		<a class="border shadow inset link-box" href="https://github.com/pierogis/live">github</a>
-		<a class="border shadow inset link-box" href="https://twitter.com/pierogis_live">
-			@pierogis_live
-		</a>
+		<Interactable>
+			<a class="border inset link-box" href="https://careers.pierogis.live">careers</a>
+		</Interactable>
+		<Interactable>
+			<a class="border inset link-box" href="https://github.com/pierogis/live">github</a>
+		</Interactable>
+		<Interactable>
+			<a class="border inset link-box" href="https://twitter.com/pierogis_live">@pierogis_live</a>
+		</Interactable>
 		<ThemeToggle />
 	</footer>
 </Layout>
 
 <style>
+	@import '@pierogis/utensils/styles/pierogis.css';
+
 	.tagline {
 		text-align: center;
 		padding: 1rem;
