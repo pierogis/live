76d4b618d9d3a10e6bd577c4539b553588790947
Kyle Moore <moore.kyle39@gmail.com>
2023-12-29 00:00:38 -0500

    stale: remove vercel/speed insights

--- sites/stale/astro.config.mjs
+++ sites/stale/astro.config.mjs
@@ -1,12 +1,8 @@
 import { defineConfig } from "astro/config";
 import svelte from "@astrojs/svelte";
 
-import vercel from "@astrojs/vercel/serverless";
-
 // https://astro.build/config
 export default defineConfig({
   integrations: [svelte()],
   site: "https://stale.pierogis.live",
-  output: "server",
-  adapter: vercel({ speedInsights: { enabled: true } }),
 });
