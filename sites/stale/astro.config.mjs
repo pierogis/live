f8182e2177e7decfdc97257985a52c631b1c6aaa
Kyle Moore <moore.kyle39@gmail.com>
2023-12-30 17:53:01 -0500

    stale: add vercel adapter

--- sites/stale/astro.config.mjs
+++ sites/stale/astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from "astro/config";
 import svelte from "@astrojs/svelte";
-
 import mdx from "@astrojs/mdx";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   output: "hybrid",
   integrations: [svelte(), mdx()],
   site: "https://stale.pierogis.live",
+  adapter: vercel()
 });
