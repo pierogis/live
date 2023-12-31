0fa1617ad269381e13746d784e57c46d2ce055d9
Kyle Moore <moore.kyle39@gmail.com>
2023-12-30 22:24:05 -0500

    emporium: provide theme

--- sites/emporium/src/routes/+layout.svelte
+++ sites/emporium/src/routes/+layout.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { Interactable } from '@pierogis/utensils';
+	import { Interactable, ThemeProvider } from '@pierogis/utensils';
 	import { Layout } from 'ui';
 
 	import './style.css';
@@ -8,30 +8,32 @@
 	$: ({ sessionUser } = data);
 </script>
 
-<Layout
-	title="karl's plate emporium"
-	github="https://github.com/pierogis/live/tree/main/sites/emporium"
->
-	<nav slot="nav">
-		<Interactable>
-			<a href="/jurisdictions" class="link-box border inset">jurisdictions</a>
-		</Interactable>
-
-		<Interactable>
-			<a class="border inset link-box" href="/plates/faq">faq</a>
-		</Interactable>
-		{#if sessionUser?.isAdmin}
+<ThemeProvider>
+	<Layout
+		title="karl's plate emporium"
+		github="https://github.com/pierogis/live/tree/main/sites/emporium"
+	>
+		<nav slot="nav">
 			<Interactable>
-				<a href="/plates/create"><h4 class="link-box border inset good">create</h4></a>
+				<a href="/jurisdictions" class="link-box border inset">jurisdictions</a>
 			</Interactable>
-		{/if}
 
-		<Interactable>
-			<a href="/account" class="link-box border inset" class:good={sessionUser === null}>
-				{sessionUser !== null ? sessionUser.serial : 'login'}
-			</a>
-		</Interactable>
-	</nav>
+			<Interactable>
+				<a class="border inset link-box" href="/plates/faq">faq</a>
+			</Interactable>
+			{#if sessionUser?.isAdmin}
+				<Interactable>
+					<a href="/plates/create"><h4 class="link-box border inset good">create</h4></a>
+				</Interactable>
+			{/if}
 
-	<slot />
-</Layout>
+			<Interactable>
+				<a href="/account" class="link-box border inset" class:good={sessionUser === null}>
+					{sessionUser !== null ? sessionUser.serial : 'login'}
+				</a>
+			</Interactable>
+		</nav>
+
+		<slot />
+	</Layout>
+</ThemeProvider>
