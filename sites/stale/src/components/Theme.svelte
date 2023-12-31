57c7f8499b4877e3b9cc5e8dc8c77e5da1989408
Kyle Moore <moore.kyle39@gmail.com>
2023-12-30 21:35:51 -0500

    stale: centralize seo and fix theme with changes to utensils

---
+++ sites/stale/src/components/Theme.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import { writable, type Writable } from "svelte/store";
+
+  import { ThemeToggle } from "@pierogis/utensils";
+  import { syncThemeAction, Theme } from "@pierogis/utensils/theme";
+
+  const storedTheme: Writable<Theme | null> = writable();
+</script>
+
+<svelte:window use:syncThemeAction={{ storedTheme }} />
+<ThemeToggle {storedTheme} />
