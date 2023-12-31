349db0628cb93bdd23816f37566f03c88fbc361d
Kyle Moore <moore.kyle39@gmail.com>
2023-12-31 02:47:44 -0500

    stale: seo

---
+++ sites/stale/src/components/SEO.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+  export let canonical: string;
+  export let title: string;
+  export let description: string;
+  export let type: string | undefined;
+  export let og:
+    | {
+        image: string;
+        alt: string;
+      }
+    | undefined = undefined;
+  export let twitter:
+    | {
+        card: "summary" | "summary_large_image";
+        image: string;
+        alt: string;
+        site: string;
+        handle?: string;
+      }
+    | undefined = undefined;
+</script>
+
+<title>{title}</title>
+<link rel="canonical" href={canonical} />
+<meta name="description" content={description} />
+<meta name="robots" content="index, follow" />
+<meta name="og:title" content={title} />
+<meta name="og:type" content={type} />
+{#if og}
+  <meta name="og:image" content={og.image} />
+  <meta name="og:image:alt" content={og.alt} />
+{/if}
+{#if twitter}
+  <meta name="twitter:title" content={title} />
+  <meta name="twitter:description" content={description} />
+  <meta name="twitter:card" content={twitter.card} />
+  <meta name="twitter:image" content={twitter.image} />
+  <meta name="twitter:image:alt" content={twitter.alt} />
+  <meta name="twitter:site" content={twitter.site} />
+  {#if twitter.handle}
+    <meta name="twitter:creator" content={twitter.handle} />
+  {/if}
+{/if}
